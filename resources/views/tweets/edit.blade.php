23f45f69e119c5f0c420ed3ac969370f62a89e46
Warren Uhrich <hello@warren.codes>
2020-02-27 20:20:00 -0700

    Initial commit.

---
+++ resources/views/tweets/edit.blade.php
@@ -0,0 +1,32 @@
+@extends('layout')
+
+@section('title')
+Edit Tweet
+@endsection
+@section('content')
+<h1>Edit Tweet Form</h1>
+<p>Use this form to edit your tweet.</p>
+<div role="alert">
+    @if ( $errors->any() )
+        <ul>
+            @foreach ( $errors->all() as $error )
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+</div>
+<form method="post" action="{{ route('tweets.update', $tweet->id) }}">
+    @csrf 
+    @method('PATCH')
+    {{-- ^^^ Cross-site request forgery protection --}}
+    <label for="message">
+        <strong>Input a Message:</strong>
+        <textarea name="message" id="message" cols="30" rows="10">{{ $tweet->message }}</textarea>
+    </label>
+    <label for="author">
+        <strong>Author Name:</strong>
+        <input type="text" name="author" id="author" value="{{ $tweet->author }}">
+    </label>
+    <input type="submit" value="Update Tweet">
+</form>
+@endsection
